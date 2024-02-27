8e0571638d64d673e2ede4190a48bd5187dd2c50
praveen <mspraveenkumar77@gmail.com>
2024-02-27 13:57:29 +0000

    Toast Error Fix

--- htdocs/template/_script.php
+++ htdocs/template/_script.php
@@ -10,5 +10,6 @@
 <script src="../js/owl.carousel.min.js"></script>
 <script src="../js/jquery.magnific-popup.min.js"></script>
 <script src="../js/aos.js"></script>
-
+<script src="../js/toast.js"></script>
 <script src="../js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
